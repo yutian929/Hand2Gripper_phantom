a368e90a57d3f5d54a1364768a6eeb9a0eb03fe3
yutian929 <yutian929@outlook.com>
2025-11-18 11:00:07 +0800

    failed to update robosuite to v1.5, try to add Arx model into v1.4

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+def test_robo_suite():
+    from robosuite.robots import register_robot_class
+    from robosuite.models.robots import Panda
+    import robosuite as suite
+    from robosuite.controllers import load_composite_controller_config
+    import mujoco
+
+
+    # @register_robot_class("WheeledRobot")
+    # class MobilePanda(Panda):
+    #     @property
+    #     def default_base(self):
+    #         return "OmronMobileBase"
+
+    #     @property
+    #     def default_arms(self):
+    #         return {"right": "Panda"}
+
+    # Create environment
+    env = suite.make(
+        env_name="Lift",
+        robots=["Arx5","Arx5"],
+        controller_configs=load_composite_controller_config(controller="BASIC"),
+        has_renderer=True,
+        has_offscreen_renderer=False,
+        render_camera="agentview",
+        use_camera_obs=False,
+        control_freq=20,
+    )
+
+    # Run the simulation, and visualize it
+    env.reset()
+    mujoco.viewer.launch(env.sim.model._model, env.sim.data._data)
+
+
 def test_python_orb_slam3():
     import cv2
     import numpy as np
@@ -523,4 +558,5 @@ def test_rgbd_vo_with_orb():
 
 if __name__ == "__main__":
     # test_python_orb_slam3()
-    test_rgbd_vo_with_orb()
+    # test_rgbd_vo_with_orb()
+    test_robo_suite()
--- phantom/process_data.py
+++ phantom/process_data.py
@@ -120,13 +120,13 @@ def process_all_demos(cfg: DictConfig, processor_classes: dict) -> None:
         print(f"----------------- {mode.upper()} PROCESSOR -----------------")
         # >>> Hand2Gripper >>> #
         if mode.upper() in (
-            # 'BBOX', 
-            # 'HAND_SEGMENTATION',
-            # 'HAND2D', 
-            # 'ARM_SEGMENTATION', 
-            # "ACTION",
-            # "SMOOTHING",
-            # "HAND_INPAINT",
+            'BBOX', 
+            'HAND_SEGMENTATION',
+            'HAND2D', 
+            'ARM_SEGMENTATION', 
+            "ACTION",
+            "SMOOTHING",
+            "HAND_INPAINT",
             # "ROBOT_INPAINT"
             ):
             continue
