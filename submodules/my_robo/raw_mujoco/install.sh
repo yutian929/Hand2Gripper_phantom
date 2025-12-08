f3b2d4236837e436484f1b4feb3cdad608c08eec
yutian929 <yutian929@outlook.com>
2025-12-08 14:18:55 +0800

    single arm controller. tested.

--- submodules/my_robo/raw_mujoco/install.sh
+++ submodules/my_robo/raw_mujoco/install.sh
@@ -2,8 +2,10 @@ cd ARX_R5_python/
 git clone https://github.com/pybind/pybind11.git && cd pybind11 && mkdir build && cd build && cmake .. && make && sudo make install
 cd ../../
 ./build.sh
-echo LD_LIBRARY_PATH=$(pwd)/bimanual/api/arx_r5_src:$LD_LIBRARY_PATH >> ~/.bashrc
-echo LD_LIBRARY_PATH=$(pwd)/bimanual/api:$LD_LIBRARY_PATH >> ~/.bashrc
-echo LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH >> ~/.bashrc
+echo "# ARX_R5_python bimanual package" >> ~/.bashrc
+# 注意 \$LD_LIBRARY_PATH
+echo "export LD_LIBRARY_PATH=$(pwd)/bimanual/api/arx_r5_src:\$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$(pwd)/bimanual/api:\$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
 pip install -e .
 cd ..
